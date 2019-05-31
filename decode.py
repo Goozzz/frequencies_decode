67aae32099633895c6cde2be060ad6d480689bd6
Evgeny Gusev <wackyy52@gmail.com>
2019-05-31 14:06:55 +0300

    Push project

---
+++ decode.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+'''
+Module for text decoding.
+'''
+
+import textstatistics
+
+
+def encode_text(text, code):
+    '''
+    Encodes the text with a substitution cipher defined with the code.
+    Argument "code" is a dictionary with characters as keys 
+    and corresponding coding characters as values. 
+    For example:
+        code = {'a': 'z', 'b': 'y' , 'c': 'x'}
+    '''
+    result = []
+    for char in text:
+        encoded_char = code.get(char, char)
+        result.append(encoded_char)
+        
+    return  u''.join(result)
+
+
+def evalutate_decoding(text, language):
+    '''
+    Evaluates how the decoded text corresponds to the language.
+    Returns estimated fitness as a float value from the range [0; 1], where
+    0 means doesn't correspond at all,
+    1 means all words are correct.
+    '''
+    fitness_sum = 0.0
+    words = textstatistics.split_to_words(text)
+    for word in words:
+        fitness_sum += language.word_fitness(word)
+    return fitness_sum / len(words)
+
+
+def decode_text(text, language):
+    '''
+    Decodes the text encoded with a substitution cipher
+    '''
+    
+    alphabet = language.get_alphabet()
+    alphabet_encode_text = textstatistics.get_char_frequencies(text)
+    alphabet_encode_text = {char: frequency for (char, frequency) in 
+                    alphabet_encode_text.items() if char in alphabet}
+    
+
+    char_to_char = dict()
+
+    for encode_char in alphabet_encode_text.keys():
+        for char in alphabet.keys():
+            if alphabet_encode_text[encode_char] == alphabet[char]:
+                char_to_char[encode_char] = char
+                alphabet_encode_text[encode_char] = -1
+                alphabet[char] = -1
+                
+
+    print(char_to_char)
+    print(len(char_to_char))
+    decoded_text = ''
+    for char in text:
+        if char in alphabet:
+            decoded_text += char_to_char[char]
+        else:
+            decoded_text += char
+    
+    return decoded_text
---
+++ textstatistics.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+'''
+Module for text statistics calculation.
+'''
+
+import collections
+import sys
+import re
+import difflib
+
+def get_item_frequencies(sequence):
+    result = collections.defaultdict(int)
+    for item in sequence:
+        result[item] += 1
+    return result
+
+def get_char_frequencies(text):
+    return get_item_frequencies(text)
+
+def split_to_words(text):
+    raw_word_list = re.split(r"\W", text, flags=re.UNICODE)
+    result = [word for word in raw_word_list if word] # Get rid of empty items
+    return result
+
+def get_word_frequencies(text):
+    return get_item_frequencies(split_to_words(text))
+
+class Languauge:
+    '''
+    Class representing particular language with alphabet and dictionary
+    '''
+
+    def __init__(self, alphabet, dictionary):
+        self.__alphabet = alphabet
+        self.__dictionary = dictionary
+
+    def get_alphabet(self):
+        return self.__alphabet
+
+    def compare(self, word1, word2):
+        return difflib.SequenceMatcher(None, word1, word2).ratio()
+
+    def word_fitness(self, word):
+        return max([self.compare(word, dict_word)
+                    for dict_word in self.__dictionary])
+
+
+def main():
+    print(split_to_words('abc ddd as'))
+    print(get_char_frequencies(u"abc"))
+    return 0
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
